f5439f7ba6c788ceb58eaec1584448b27c95d17b
szarkarbaeva <szarkarbaeva@gmail.com>
2025-06-13 06:23:38 +0000

    new files

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin
+kubectl version --client
+kubectl get podes
+aws eks --region us-east-1 update-kubeconfig --name my-cluster-eks
